aae7079d6fc24095880da180efbe3a97f0caa73d
Jackson Stepka <jvsdestroyer25@gmail.com>
2024-11-04 08:27:17 -0700

    Adds pin map

--- src/main.c
+++ src/main.c
@@ -1,12 +1,12 @@
-#include "imxrt_regmap.h"
+#include "connectivity/gpio.h"
 // #include "imxrt.h"
 
 // TODO create actual main, this is just to satisfy linker references
 int main(void) {
 	// set default configuration for GPIO_B0_03
-	IOMUXC_SW_MUX_CTL_PAD_GPIO_B0_03->MUX_MODE = 5;
+	gpio_pin_mux_map[13]->MUX_MODE = 5;
 	// set default configuration for GPIO_B0_03
-	IOMUXC_SW_PAD_CTL_PAD_GPIO_B0_03->DSE = 7;
+	gpio_pin_pad_map[13]->DSE = 7;
 	// enable output for GPIO_B0_03
 	GPIO7->GDIR.GDIR |= (1<<3);
 	// toggle GPIO_B0_03
